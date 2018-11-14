d91286d0c59f877e7935594d0535cd328b20d6b1
Zack Jackson <z.jackson58@gmail.com>
2018-11-13 19:52:02 -0500

    Mongoose setup with mlab dev db

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true }
+);
 
 const app = express();
 
