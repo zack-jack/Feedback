b30512b160d858e1ad15517623593ce4616ff4ac
Zack Jackson <z.jackson58@gmail.com>
2018-11-13 20:35:40 -0500

    Mongoose User model

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-require('./services/passport');
-const authRoutes = require('./routes/authRoutes');
 const keys = require('./config/keys');
+const authRoutes = require('./routes/authRoutes');
+
+require('./models/User');
+require('./services/passport');
 
 mongoose.connect(
   keys.mongoURI,
--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +14,7 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
+      new User({ googleID: profile.id }).save();
     }
   )
 );
