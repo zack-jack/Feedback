b7beba30665515127ab5eef646467da44044a1c0
Zack Jackson <z.jackson58@gmail.com>
2018-11-14 15:21:52 -0500

    Serialize and Deserialize user with passport

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const authRoutes = require('./routes/authRoutes');
 require('./models/User');
 require('./services/passport');
 
+// IMPORTANT!!!
+// DELETE this before production build.
+// Allows use of localhost without SSL for testing of OAuth in development
+require('https').globalAgent.options.rejectUnauthorized = false;
+
 mongoose.connect(
   keys.mongoURI,
   { useNewUrlParser: true }
--- services/passport.js
+++ services/passport.js
@@ -6,6 +6,14 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -14,7 +22,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleID: profile.id }).save();
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // record with given profile ID already exists
+          done(null, existingUser);
+        } else {
+          // user with this ID doesn't exist, make a new record
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
