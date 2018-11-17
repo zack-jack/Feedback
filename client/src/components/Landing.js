7a4f26200dd4315329a1d179ce75a91c956942ee
Zack Jackson <z.jackson58@gmail.com>
2018-11-17 12:29:58 -0500

    Landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h1>Feedback</h1>
+      <p>Flippin' fast feedback from your users!</p>
+    </div>
+  );
+};
+
+export default Landing;
