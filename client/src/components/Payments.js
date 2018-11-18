0097289de9e3878395643aba334a9c5712e7d40a
Zack Jackson <z.jackson58@gmail.com>
2018-11-17 21:49:39 -0500

    Payments component

---
+++ client/src/components/Payments.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Feedback"
+        description="$5 for 5 survey credits"
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn white red-text text-lighten-1">
+          Add Credits
+        </button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default Payments;
