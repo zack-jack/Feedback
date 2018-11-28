205a0ee41932153dd80824d2e22e4f29d5c75784
Zack Jackson <z.jackson58@gmail.com>
2018-11-27 20:50:42 -0500

    Fetch surveys action and surveys reducer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,6 +11,14 @@ const Mailer = require('../services/Mailer');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+
+    res.send(surveys);
+  });
+
   app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks so much for your feedback!');
   });
